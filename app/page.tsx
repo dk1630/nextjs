dcddd7ba7f663042e5d74f7027785cca81623504
Mayur Bariya <mayur@membio.com>
2024-05-02 18:34:40 +0530

    fix ads

--- app/page.tsx
+++ app/page.tsx
@@ -19,9 +19,9 @@ export default function Home() {
           </a>
         </div>
       </div>
+      <AdComponent />
       <h1 className="text-4xl font-extrabold">OnlineZones Game Demo</h1>
       {/* Include AdComponent where appropriate */}
-      <AdComponent />
     </main>
   );
 }
--- components/AddAdvertisements.tsx
+++ components/AddAdvertisements.tsx
@@ -1,22 +1,32 @@
 "use client"
+import Head from 'next/head';
 import React, { useEffect } from 'react';
 
 const AdComponent = () => {
 
-  // useEffect(() => {
-  //   try {
-  //     (window.adsbygoogle = window.adsbygoogle || []).push({});
-  //   } catch (err) {
-  //     console.log(err);
-  //   }
-  // }, []);
+  useEffect(() => {
+    try {
+      if (window.adsbygoogle) {
+        (window.adsbygoogle = window.adsbygoogle || []).push({});
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  }, []);
+
   return (
-    <ins className="adsbygoogle"
-         style={{ display: 'block' }}
-         data-ad-client="ca-pub-5905842999139001"
-         data-ad-slot="7996162986"
-         data-ad-format="auto"
-         data-full-width-responsive="true"></ins>
+    <>
+      <Head>
+        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5905842999139001"
+          crossOrigin="anonymous"></script>
+      </Head>
+      <ins className="adsbygoogle"
+        style={{ display: "block",color:"black" }}
+        data-ad-client="ca-pub-5905842999139001"
+        data-ad-slot="7996162986"
+        data-ad-format="auto"
+        data-full-width-responsive="true"></ins>
+    </>
   );
 };
 
