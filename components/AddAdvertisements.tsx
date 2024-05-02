28669639c739eb101d5209c1d11df4115ddf1f46
Mayur Bariya <mayur@membio.com>
2024-05-02 19:08:33 +0530

    make some changes for run add

--- components/AddAdvertisements.tsx
+++ components/AddAdvertisements.tsx
@@ -6,22 +6,23 @@ const AdComponent = () => {
   const [isAdLoaded, setIsAdLoaded] = useState(false);
 
   useEffect(() => {
-    if (!isAdLoaded && window.adsbygoogle) {
+    if (!isAdLoaded && window.adsbygoogle && typeof window !== 'undefined') {
       try {
-        (window.adsbygoogle = window.adsbygoogle || []).push({});
-        setIsAdLoaded(true); // Set to true after successfully loading the ad
+        window.adsbygoogle = window.adsbygoogle || [];
+        window.adsbygoogle.push({});
+        setIsAdLoaded(true); // Prevent further ad loading attempts
       } catch (err) {
-        console.log(err);
+        console.error('Error loading ad:', err);
       }
     }
-  }, [isAdLoaded]); 
+  }, [isAdLoaded]);
 
   return (
     <>
-        {/* <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5905842999139001"
-          crossOrigin="anonymous"></script> */}
+      <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5905842999139001"
+        crossOrigin="anonymous"></script>
       <ins className="adsbygoogle"
-        style={{ display: "block",}}
+        style={{ display: "block", backgroundColor: "black" }}
         data-ad-client="ca-pub-5905842999139001"
         data-ad-slot="7996162986"
         data-ad-format="auto"
