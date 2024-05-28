ac936ad9af6068de8d9af0458b4daa387ed83b29
dk1630 <168711883+dk1630@users.noreply.github.com>
2024-05-28 17:17:37 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
   
   return (
     <>
-     {/* <head> */}
+     <Head>
         <title>FSSS OnlineZones Game Demo 12n</title>
         <meta name="description" content="Discover the best online games at Online Game Zones. Play and enjoy a wide variety of games for all ages!" />
         <meta name="keywords" content={` FSSS, OnlineZones, Game, Demo, 12,`} />
@@ -17,7 +17,7 @@ export default function Home() {
         <meta property="og:image" content="https://imgupscaler.com/images/samples/Imgupscaler_1_2x.webp" />
         <link rel="canonical" href={`https://nextjs-seven-rosy-32.vercel.app/`} />
         <meta name="google-site-verification" content="Fsk2xaeXXp_K1UQNwk1RObnbvrq9HOxCpcm2uSzbx5U" />
-      {/* </head> */}
+      </Head>
       <div className="bg-black">
         <img src="https://imgupscaler.com/images/samples/Imgupscaler_1_2x.webp" />
       </div>
