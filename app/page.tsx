556d0a00f3f3154a1576adb073df5bf5113eeaa2
dk1630 <168711883+dk1630@users.noreply.github.com>
2024-05-28 13:09:57 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -7,21 +7,21 @@ export default function Home() {
   return (
     <>
      {/* <head> */}
-        <title>Online Game Zones - Best Online Games Collection</title>
+        <title>FSSS OnlineZones Game Demo 12n</title>
         <meta name="description" content="Discover the best online games at Online Game Zones. Play and enjoy a wide variety of games for all ages!" />
-        <meta name="keywords" content={` online games, free games, action games, puzzle games, strategy games`} />
-        <meta property="og:title" content="Online Game Zones - Best Online Games Collection" />
+        <meta name="keywords" content={` FSSS OnlineZones Game Demo 12`} />
+        <meta property="og:title" content="FSSS OnlineZones Game Demo 12" />
         <meta property="og:description" content="Discover the best online games at Online Game Zones. Play and enjoy a wide variety of games for all ages!" />
         <meta property="og:url" content={`https://nextjs-seven-rosy-32.vercel.app/`} />
         <meta property="og:type" content="website" />
         <meta property="og:image" content="https://nextjs-seven-rosy-32.vercel.app/" />
         <link rel="canonical" href={`https://nextjs-seven-rosy-32.vercel.app/`} />
-{/*         <meta name="google-site-verification" content="Fsk2xaeXXp_K1UQNwk1RObnbvrq9HOxCpcm2uSzbx5U" /> */}
+        <meta name="google-site-verification" content="Fsk2xaeXXp_K1UQNwk1RObnbvrq9HOxCpcm2uSzbx5U" />
       {/* </head> */}
       <div className="bg-black">
         <img src="https://imgupscaler.com/images/samples/Imgupscaler_1_2x.webp" />
       </div>
-      <h1 className="text-4xl font-extrabold">OnlineZones Game OG</h1>
+      <h1 className="text-4xl font-extrabold">FSSS OnlineZones Game Demo 12</h1>
     </>
   );
 }
