cb8bd2d335d1e793501792db11e7a512d4e1b07b
dk1630 <168711883+dk1630@users.noreply.github.com>
2024-05-28 13:05:04 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -16,7 +16,7 @@ export default function Home() {
         <meta property="og:type" content="website" />
         <meta property="og:image" content="https://nextjs-seven-rosy-32.vercel.app/" />
         <link rel="canonical" href={`https://nextjs-seven-rosy-32.vercel.app/`} />
-        <meta name="google-site-verification" content="Fsk2xaeXXp_K1UQNwk1RObnbvrq9HOxCpcm2uSzbx5U" />
+{/*         <meta name="google-site-verification" content="Fsk2xaeXXp_K1UQNwk1RObnbvrq9HOxCpcm2uSzbx5U" /> */}
       {/* </head> */}
       <div className="bg-black">
         <img src="https://imgupscaler.com/images/samples/Imgupscaler_1_2x.webp" />
