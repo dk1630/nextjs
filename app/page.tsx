6cff3e131a79fa963c2671411b42f6b18f659483
dk1630 <168711883+dk1630@users.noreply.github.com>
2024-05-28 17:46:09 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -6,18 +6,18 @@ export default function Home() {
   
   return (
     <>
-     <Head>
-        <title>FSSS OnlineZones Game Demo 12n</title>
+     {/* <head> */}
+        <title>ONLINE GAMER XONES</title>
         <meta name="description" content="Discover the best online games at Online Game Zones. Play and enjoy a wide variety of games for all ages!" />
         <meta name="keywords" content={` FSSS, OnlineZones, Game, Demo, 12,`} />
-        <meta property="og:title" content="FSSS OnlineZones Game Demo 12" />
+        <meta property="og:title" content="ONLINE GAMER XONES" />
         <meta property="og:description" content="Discover the best online games at Online Game Zones. Play and enjoy a wide variety of games for all ages!" />
         <meta property="og:url" content={`https://nextjs-seven-rosy-32.vercel.app/`} />
-{/*         <meta property="og:type" content="website" /> */}
+        <meta property="og:type" content="website" />
         <meta property="og:image" content="https://imgupscaler.com/images/samples/Imgupscaler_1_2x.webp" />
         <link rel="canonical" href={`https://nextjs-seven-rosy-32.vercel.app/`} />
         <meta name="google-site-verification" content="Fsk2xaeXXp_K1UQNwk1RObnbvrq9HOxCpcm2uSzbx5U" />
-      </Head>
+      {/* </head> */}
       <div className="bg-black">
         <img src="https://imgupscaler.com/images/samples/Imgupscaler_1_2x.webp" />
       </div>
