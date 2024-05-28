f672f379330696697433d49f3cecd721057b3591
dk1630 <168711883+dk1630@users.noreply.github.com>
2024-05-28 11:40:23 +0530

    Update page.tsx
    Add new meta

--- app/page.tsx
+++ app/page.tsx
@@ -5,38 +5,14 @@ import Head from "next/head";
 
 export default function Home() {
   return (
-    // <main className="flex min-h-screen flex-col items-center justify-between p-24">
-    //   <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-    //     <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-    //       Get started by editing&nbsp;<code className="font-mono font-bold">app/page.tsx</code>
-    //     </p>
-    //     <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-    //       <a
-    //         className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-    //         href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-    //         target="_blank"
-    //         rel="noopener noreferrer"
-    //       >
-    //         By Game Demo
-    //       </a>
-    //     </div>
-    //   </div>
     <>
       <Head>
-        <title>Testing Site</title>
-         <meta name="google-site-verification" content="Fsk2xaeXXp_K1UQNwk1RObnbvrq9HOxCpcm2uSzbx5U" />
+        <title>Testing Site OnlineZones</title>
+        <meta name="google-site-verification" content="Fsk2xaeXXp_K1UQNwk1RObnbvrq9HOxCpcm2uSzbx5U" />
       </Head>
-       {/* <Script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5905842999139001"
-        crossOrigin="anonymous"></Script> */}
       <div className="bg-black">
-        {/* <AdComponent 
-        data-ad-slot="5649201243"
-          data-ad-format="auto"
-          data-full-width-responsive="true" /> */}
-          <AdComponent/>
       </div>
-      <h1 className="text-4xl font-extrabold">OnlineZones Game Demo</h1>
+      <h1 className="text-4xl font-extrabold">OnlineZones Game Demo 12</h1>
     </>
-    // </main>
   );
 }
