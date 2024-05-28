6f36d794c61b299e2d933e4f97fc9f642c5d3949
dk1630 <168711883+dk1630@users.noreply.github.com>
2024-05-28 11:44:34 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -6,8 +6,16 @@ import Head from "next/head";
 export default function Home() {
   return (
     <>
-      <Head>
-        <title>Testing Site OnlineZones</title>
+     <Head>
+        <title>Online Game Zones - Best Online Games Collection</title>
+        <meta name="description" content="Discover the best online games at Online Game Zones. Play and enjoy a wide variety of games for all ages!" />
+        <meta name="keywords" content={` online games, free games, action games, puzzle games, strategy games`} />
+        <meta property="og:title" content="Online Game Zones - Best Online Games Collection" />
+        <meta property="og:description" content="Discover the best online games at Online Game Zones. Play and enjoy a wide variety of games for all ages!" />
+        <meta property="og:url" content={`https://nextjs-seven-rosy-32.vercel.app/`} />
+        <meta property="og:type" content="website" />
+        <meta property="og:image" content="https://nextjs-seven-rosy-32.vercel.app/" />
+        <link rel="canonical" href={`https://nextjs-seven-rosy-32.vercel.app/`} />
         <meta name="google-site-verification" content="Fsk2xaeXXp_K1UQNwk1RObnbvrq9HOxCpcm2uSzbx5U" />
       </Head>
       <div className="bg-black">
