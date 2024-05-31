57fbe7b1d1cd0952a45bb02b99887fbe6dc0f1b4
dk1630 <168711883+dk1630@users.noreply.github.com>
2024-05-31 12:58:48 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -28,28 +28,30 @@ export default function Home() {
      <head>
        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>TESTSING DOMIN AND MY WEB IN GOOGLE NEXT SEVEN ROSY BY KUNAL D</title>
-        <meta name="description" content="Discover the best online games at Online Game Zones. Play and enjoy a wide variety of games for all ages!" />
-        <meta name="title" content="TESTSING DOMIN AND MY WEB IN GOOGLE NEXT SEVEN ROSY BY KUNAL D" />
-        <meta name="keywords" content={` FSSS, OnlineZones, Game, Demo, 12,`} />
-        <meta property="og:title" content="TESTSING DOMIN AND MY WEB IN GOOGLE NEXT SEVEN ROSY BY KUNAL D" />
-        <meta property="og:description" content="Discover the best online games at Online Game Zones. Play and enjoy a wide variety of games for all ages!" />
-        <meta property="og:type" content="website" />
-        <meta property="og:site_name" content="TESTSING DOMIN AND MY WEB IN GOOGLE NEXT SEVEN ROSY BY KUNAL D" />
-        <meta property="og:url" content="article" />
-        <meta property="og:image" content="https://imgupscaler.com/images/samples/Imgupscaler_1_2x.webp" />
-       <meta name="twitter:card" content="summary" />
-    <meta name="twitter:title" content="TESTSING DOMIN AND MY WEB IN GOOGLE NEXT SEVEN ROSY BY KUNAL D" />
+        <meta name="description" content="Discover the best online games at Online Game Zones. Play and enjoy a wide variety of games for all ages!" data-react-helmet="true"  />
+        <meta name="title" content="TESTSING DOMIN AND MY WEB IN GOOGLE NEXT SEVEN ROSY BY KUNAL D" data-react-helmet="true" />
+        <meta name="keywords" content={` FSSS, OnlineZones, Game, Demo, 12,`} data-react-helmet="true" />
+        <meta property="og:title" content="TESTSING DOMIN AND MY WEB IN GOOGLE NEXT SEVEN ROSY BY KUNAL D" data-react-helmet="true" />
+        <meta property="og:description" content="Discover the best online games at Online Game Zones. Play and enjoy a wide variety of games for all ages!" data-react-helmet="true" />
+        <meta property="og:type" content="website" data-react-helmet="true"/>
+        <meta property="og:site_name" content="TESTSING DOMIN AND MY WEB IN GOOGLE NEXT SEVEN ROSY BY KUNAL D" data-react-helmet="true" />
+        <meta property="og:url" content="article" data-react-helmet="true" />
+        <meta property="og:image" content="https://imgupscaler.com/images/samples/Imgupscaler_1_2x.webp" data-react-helmet="true" />
+       <meta name="twitter:card" content="summary" data-react-helmet="true" />
+    <meta name="twitter:title" content="TESTSING DOMIN AND MY WEB IN GOOGLE NEXT SEVEN ROSY BY KUNAL D" data-react-helmet="true" />
     <meta
       name="twitter:image"
       content="https://imgupscaler.com/images/samples/Imgupscaler_1_2x.webp"
+     data-react-helmet="true" 
     />
     <meta
       name="twitter:description"
       content="Discover the best online games at Online Game Zones. Play and enjoy a wide variety of games for all ages!"
+     data-react-helmet="true" 
     />
-    <meta name="twitter:site" content="https://nextjs-seven-rosy-32.vercel.app/" />
-        <link rel="canonical" href={`https://nextjs-seven-rosy-32.vercel.app/`} />
-        <meta name="google-site-verification" content="Fsk2xaeXXp_K1UQNwk1RObnbvrq9HOxCpcm2uSzbx5U" />
+    <meta name="twitter:site" content="https://nextjs-seven-rosy-32.vercel.app/" data-react-helmet="true" />
+        <link rel="canonical" href={`https://nextjs-seven-rosy-32.vercel.app/`} data-react-helmet="true" />
+        <meta name="google-site-verification" content="Fsk2xaeXXp_K1UQNwk1RObnbvrq9HOxCpcm2uSzbx5U" data-react-helmet="true"  />
         <script type="application/ld+json" dangerouslySetInnerHTML={{ __html: JSON.stringify(articleStructuredData) }} />
       </head>
       <div className="bg-black flex">
