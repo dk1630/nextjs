39ab68363e51cc24effb3dd17d85c1ef17e2de7a
dk1630 <168711883+dk1630@users.noreply.github.com>
2024-05-31 11:35:40 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -32,17 +32,13 @@ export default function Home() {
         <meta property="og:image" content="https://imgupscaler.com/images/samples/Imgupscaler_1_2x.webp" />
         <link rel="canonical" href={`https://nextjs-seven-rosy-32.vercel.app/`} />
         <meta name="google-site-verification" content="Fsk2xaeXXp_K1UQNwk1RObnbvrq9HOxCpcm2uSzbx5U" />
-      <script type="application/ld+json" dangerouslySetInnerHTML={{ __html: JSON.stringify(articleStructuredData) }} />
+        <script type="application/ld+json" dangerouslySetInnerHTML={{ __html: JSON.stringify(articleStructuredData) }} />
       </head>
-
-
-    <!-- Add structured data -->
-   
       <div className="bg-black flex">
         <img src="https://imgupscaler.com/images/samples/Imgupscaler_1_2x.webp" />
         <img src="https://imgupscaler.com/images/samples/Imgupscaler_1_2x.webp" />
       </div>
-      <h1 className="text-4xl font-extrabold">ONLINE GAMER XONES</h1>
+      <h1 className="text-4xl font-extrabold">TESTSING DOMIN AND MY WEB IN GOOGLE NEXT SEVEN ROSY</h1>
     </>
   );
 }
