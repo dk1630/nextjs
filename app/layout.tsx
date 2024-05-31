793b6f9777b11d35f29eda9372adfb13bc728e5f
dk1630 <168711883+dk1630@users.noreply.github.com>
2024-05-31 11:30:44 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,6 +12,19 @@ export default async function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const articleStructuredData = {
+    "@context": "https://schema.org",
+    "@type": "Article",
+    "headline": "ONLINE GAMER XONES 2",
+    "description": "ONLINE GAMER XONES 2 , This is an article that demonstrates structured data.",
+    "image": "https://imgupscaler.com/images/samples/Imgupscaler_1_2x.webp",
+    "datePublished": "2020-02-11T06:42:03.706Z",
+    "author": {
+      "@type": "Person",
+      "name": "John Reilly",
+      "url": "https://nextjs-seven-rosy-32.vercel.app/"
+    }
+  }
   return (
     <html lang="en">
       {/* <Script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5905842999139001"
@@ -19,18 +32,19 @@ export default async function RootLayout({
       <Head>
       
         <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>TESTSING DOMIN AND MY WEB IN GOOGLE NEXT SEVEN ROSY</title>
-    <meta name="description" content="TESTING DOMIN AND MY WEB IN GOOGLE, AND IT"S WORKING YAY HURRAY!">
-    <meta name="keywords" content="TESTING DOMIN AND MY WEB IN GOOGLE, AND IT"S WORKING YAY HURRAY!">
+    <meta name="description" content="TESTING DOMIN AND MY WEB IN GOOGLE, AND IT"S WORKING YAY HURRAY!"/>
+    <meta name="keywords" content="TESTING DOMIN AND MY WEB IN GOOGLE, AND IT"S WORKING YAY HURRAY!"/>
     <meta name="robots" content="index,follow">
-    <meta property="og:title" content="TESTSING DOMIN AND MY WEB IN GOOGLE NEXT SEVEN ROSY">
-    <meta property="og:description" content="TESTING DOMIN AND MY WEB IN GOOGLE, AND IT"S WORKING YAY HURRAY!">
-    <meta property="og:url" content="https://nextjs-seven-rosy-32.vercel.app/">
-    <meta property="og:type" content="website">
-    <meta property="og:image" content="https://imgupscaler.com/images/samples/Imgupscaler_1_2x.webp">
-    <link rel="canonical" href="https://nextjs-seven-rosy-32.vercel.app/">
-    <meta name="google-site-verification" content="Fsk2xaeXXp_K1UQNwk1RObnbvrq9HOxCpcm2uSzbx5U">
+    <meta property="og:title" content="TESTSING DOMIN AND MY WEB IN GOOGLE NEXT SEVEN ROSY"/>
+    <meta property="og:description" content="TESTING DOMIN AND MY WEB IN GOOGLE, AND IT"S WORKING YAY HURRAY!"/>
+    <meta property="og:url" content="https://nextjs-seven-rosy-32.vercel.app/"/>
+    <meta property="og:type" content="website"/>
+    <meta property="og:image" content="https://imgupscaler.com/images/samples/Imgupscaler_1_2x.webp"/>
+    <link rel="canonical" href="https://nextjs-seven-rosy-32.vercel.app/"/>
+    <meta name="google-site-verification" content="Fsk2xaeXXp_K1UQNwk1RObnbvrq9HOxCpcm2uSzbx5U"/>
+       <script type="application/ld+json" dangerouslySetInnerHTML={{ __html: JSON.stringify(articleStructuredData) }} />
       </Head>
         
       {/* <meta name="google-adsense-account" content="ca-pub-5905842999139001" /> */}
