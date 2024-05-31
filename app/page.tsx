40b36281f40a4e578f463abcb453ce638eb27037
dk1630 <168711883+dk1630@users.noreply.github.com>
2024-05-31 11:32:52 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -8,20 +8,20 @@ export default function Home() {
   
   return (
     <>
-     {/* <head> */}
-{/*         <title>ONLINE GAMER XONES</title>
+     <head>
+        <title>TESTSING DOMIN AND MY WEB IN GOOGLE NEXT SEVEN ROSY</title>
         <meta name="description" content="Discover the best online games at Online Game Zones. Play and enjoy a wide variety of games for all ages!" />
-        <meta name="title" content="ONLINE GAMER XONES" />
+        <meta name="title" content="TESTSING DOMIN AND MY WEB IN GOOGLE NEXT SEVEN ROSY" />
         <meta name="keywords" content={` FSSS, OnlineZones, Game, Demo, 12,`} />
-        <meta property="og:title" content="ONLINE GAMER XONES" />
+        <meta property="og:title" content="TESTSING DOMIN AND MY WEB IN GOOGLE NEXT SEVEN ROSY" />
         <meta property="og:description" content="Discover the best online games at Online Game Zones. Play and enjoy a wide variety of games for all ages!" />
         <meta property="og:url" content={`https://nextjs-seven-rosy-32.vercel.app/`} />
         <meta property="og:type" content="website" />
         <meta property="og:image" content="https://imgupscaler.com/images/samples/Imgupscaler_1_2x.webp" />
         <link rel="canonical" href={`https://nextjs-seven-rosy-32.vercel.app/`} />
         <meta name="google-site-verification" content="Fsk2xaeXXp_K1UQNwk1RObnbvrq9HOxCpcm2uSzbx5U" />
-      <script type="application/ld+json" dangerouslySetInnerHTML={{ __html: JSON.stringify(articleStructuredData) }} /> */}
-      {/* </head> */}
+      <script type="application/ld+json" dangerouslySetInnerHTML={{ __html: JSON.stringify(articleStructuredData) }} />
+      </head>
 
 
     <!-- Add structured data -->
