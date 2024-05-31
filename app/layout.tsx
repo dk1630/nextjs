b7dc5a5d359f31ebba765da72734695087759c8d
dk1630 <168711883+dk1630@users.noreply.github.com>
2024-05-31 11:33:45 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,25 +27,20 @@ export default async function RootLayout({
   }
   return (
     <html lang="en">
-      {/* <Script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5905842999139001"
-        crossOrigin="anonymous"></Script> */}
-      <Head>
-      
-        <meta charset="UTF-8"/>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>TESTSING DOMIN AND MY WEB IN GOOGLE NEXT SEVEN ROSY</title>
-    <meta name="description" content="TESTING DOMIN AND MY WEB IN GOOGLE, AND IT"S WORKING YAY HURRAY!"/>
-    <meta name="keywords" content="TESTING DOMIN AND MY WEB IN GOOGLE, AND IT"S WORKING YAY HURRAY!"/>
-    <meta name="robots" content="index,follow"/>
-    <meta property="og:title" content="TESTSING DOMIN AND MY WEB IN GOOGLE NEXT SEVEN ROSY"/>
-    <meta property="og:description" content="TESTING DOMIN AND MY WEB IN GOOGLE, AND IT"S WORKING YAY HURRAY!"/>
-    <meta property="og:url" content="https://nextjs-seven-rosy-32.vercel.app/"/>
-    <meta property="og:type" content="website"/>
-    <meta property="og:image" content="https://imgupscaler.com/images/samples/Imgupscaler_1_2x.webp"/>
-    <link rel="canonical" href="https://nextjs-seven-rosy-32.vercel.app/"/>
-    <meta name="google-site-verification" content="Fsk2xaeXXp_K1UQNwk1RObnbvrq9HOxCpcm2uSzbx5U"/>
-       <script type="application/ld+json" dangerouslySetInnerHTML={{ __html: JSON.stringify(articleStructuredData) }} />
-      </Head>
+      <head>
+        <title>TESTSING DOMIN AND MY WEB IN GOOGLE NEXT SEVEN ROSY</title>
+        <meta name="description" content="Discover the best online games at Online Game Zones. Play and enjoy a wide variety of games for all ages!" />
+        <meta name="title" content="TESTSING DOMIN AND MY WEB IN GOOGLE NEXT SEVEN ROSY" />
+        <meta name="keywords" content={` FSSS, OnlineZones, Game, Demo, 12,`} />
+        <meta property="og:title" content="TESTSING DOMIN AND MY WEB IN GOOGLE NEXT SEVEN ROSY" />
+        <meta property="og:description" content="Discover the best online games at Online Game Zones. Play and enjoy a wide variety of games for all ages!" />
+        <meta property="og:url" content={`https://nextjs-seven-rosy-32.vercel.app/`} />
+        <meta property="og:type" content="website" />
+        <meta property="og:image" content="https://imgupscaler.com/images/samples/Imgupscaler_1_2x.webp" />
+        <link rel="canonical" href={`https://nextjs-seven-rosy-32.vercel.app/`} />
+        <meta name="google-site-verification" content="Fsk2xaeXXp_K1UQNwk1RObnbvrq9HOxCpcm2uSzbx5U" />
+      <script type="application/ld+json" dangerouslySetInnerHTML={{ __html: JSON.stringify(articleStructuredData) }} />
+      </head>
         
       {/* <meta name="google-adsense-account" content="ca-pub-5905842999139001" /> */}
       <body className={inter.className}>{children}</body>
