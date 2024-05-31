57d4421d07831972ff74e9eed4e0d9615dbcc4a4
dk1630 <168711883+dk1630@users.noreply.github.com>
2024-05-31 11:28:49 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,20 +12,6 @@ export default async function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-
-  const articleStructuredData = {
-    "@context": "https://schema.org",
-    "@type": "Article",
-    "headline": "TESTSING DOMIN AND MY WEB IN GOOGLE NEXT SEVEN ROSY",
-    "description": "ONLINE GAMER XONES 2 , This is an article that demonstrates structured data.",
-    "image": "https://imgupscaler.com/images/samples/Imgupscaler_1_2x.webp",
-    "datePublished": "2020-02-11T06:42:03.706Z",
-    "author": {
-      "@type": "Person",
-      "name": "John Reilly",
-      "url": "https://nextjs-seven-rosy-32.vercel.app/"
-    }
-  }
   return (
     <html lang="en">
       {/* <Script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5905842999139001"
@@ -45,7 +31,6 @@ export default async function RootLayout({
     <meta property="og:image" content="https://imgupscaler.com/images/samples/Imgupscaler_1_2x.webp">
     <link rel="canonical" href="https://nextjs-seven-rosy-32.vercel.app/">
     <meta name="google-site-verification" content="Fsk2xaeXXp_K1UQNwk1RObnbvrq9HOxCpcm2uSzbx5U">
-       <script type="application/ld+json" dangerouslySetInnerHTML={{ __html: JSON.stringify(articleStructuredData) }} />
       </Head>
         
       {/* <meta name="google-adsense-account" content="ca-pub-5905842999139001" /> */}
