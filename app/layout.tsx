2d2a984a1ab651e2c7641821d8e4cbcc9ce9704c
dk1630 <168711883+dk1630@users.noreply.github.com>
2024-06-08 09:15:36 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -39,6 +39,9 @@ export default async function RootLayout({
         <meta property="og:image" content="https://imgupscaler.com/images/samples/Imgupscaler_1_2x.webp" />
         <link rel="canonical" href={`https://nextjs-seven-rosy-32.vercel.app/`} />
        <meta name="google-site-verification" content="Fsk2xaeXXp_K1UQNwk1RObnbvrq9HOxCpcm2uSzbx5U" />
+        
+      <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5905842999139001"
+     crossOrigin="anonymous"></script>
       <script type="application/ld+json" dangerouslySetInnerHTML={{ __html: JSON.stringify(articleStructuredData) }} />
       </Head>
         
